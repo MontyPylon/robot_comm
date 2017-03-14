b3936c953be81afba3baebff006e8d0e00fb3f0a
Jeremy Cook <jeremycook@utexas.edu>
2017-03-14 13:15:09 -0500

    New teleop tests
    Signed-off-by: Jeremy Cook <jeremycook@utexas.edu>

--- src/RobotMain.cpp
+++ src/RobotMain.cpp
@@ -2,24 +2,105 @@
 #include "std_msgs/String.h"
 #include <robot_comm/Motor.h>
 #include <sstream>
+#include <signal.h>
+#include <termios.h>
+
+#define KEYCODE_R 0x43
+#define KEYCODE_L 0x44
+#define KEYCODE_U 0x41
+#define KEYCODE_D 0x42
+#define KEYCODE_Q 0x71
+
+using namespace std;
+
+int kfd = 0;
+struct termios cooked, raw;
+
+void quit(int sig) {
+    (void) sig;
+    tcsetattr(kfd, TCSANOW, &cooked);
+    ros::shutdown();
+    exit(0);
+}
+
+void teleOpCommands() {
+    char c;
+    bool dirty = false;
+
+    // get the console in raw mode                                                              
+    tcgetattr(kfd, &cooked);
+    memcpy(&raw, &cooked, sizeof(struct termios));
+    raw.c_lflag &= ~(ICANON | ECHO);
+    // Setting a new line, then end of file                         
+    raw.c_cc[VEOL] = 1;
+    raw.c_cc[VEOF] = 2;
+    tcsetattr(kfd, TCSANOW, &raw);
+
+    puts("Reading from keyboard");
+    puts("---------------------------");
+    puts("Use arrow keys to move the turtle.");
+
+
+    //for (;;) {
+        // get the next event from the keyboard  
+        if (read(kfd, &c, 1) < 0) {
+            perror("read():");
+            exit(-1);
+        }
+
+        switch (c) {
+            case KEYCODE_L:
+                cout << "LEFT" << endl;
+                dirty = true;
+                break;
+            case KEYCODE_R:
+                cout << "RIGHT" << endl;
+                dirty = true;
+                break;
+            case KEYCODE_U:
+                cout << "UP" << endl;
+                dirty = true;
+                break;
+            case KEYCODE_D:
+                cout << "DOWN" << endl;
+                dirty = true;
+                break;
+        }
+
+        /**
+        geometry_msgs::Twist twist;
+        twist.angular.z = a_scale_ * angular_;
+        twist.linear.x = l_scale_ * linear_;
+        if (dirty == true) {
+            twist_pub_.publish(twist);
+            dirty = false;
+        }
+         **/
+    //}
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "robot_main");
     ros::NodeHandle n;
     
     ros::Publisher command_pub = n.advertise<robot_comm::Motor>("motor", 1000);
-    ros::Rate loop_rate(10);
-    
+    ros::Rate loop_rate(1);
+    int count = 255;
     while (ros::ok())
     {
         robot_comm::Motor msg;
         msg.name = "Robot 1";
-        msg.left_power = 255;
+        msg.left_power = count;
         msg.right_power = 255;
+        
+        teleOpCommands();
+        signal(SIGINT, quit);
         
         command_pub.publish(msg);
         ros::spinOnce();
         loop_rate.sleep();
+        count--;
     }
     return 0;
 }
--- src/SendCommand.cpp
+++ src/SendCommand.cpp
@@ -1,20 +1,10 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include <sstream>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <memory.h>
-#include <ifaddrs.h>
-#include <net/if.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <iostream>
+#include <robot_comm/Motor.h>
 
 using namespace std;
 
@@ -36,7 +26,36 @@ int resolvehelper(const char* hostname, int family, const char* service, sockadd
     return result;
 }
 
-void sendMsg() {
+string returnCommand(robot_comm::Motor incoming) {
+
+    if(incoming.right_power > 255 || incoming.right_power < -255 || incoming.left_power > 255 || incoming.left_power < -255) {
+        cout << "ONE POWER IS TOO LARGE" << endl;
+        cout << "SENDING THIS COMMAND: +000+000" << endl;
+        return "+000+000";
+    }
+    
+    std::stringstream left;
+    if(incoming.left_power >= 0) {
+        left << "+" << incoming.left_power;
+    } else {
+        left << incoming.left_power;
+    }
+
+    std::stringstream right;
+    if(incoming.right_power >= 0) {
+        right << "+" << incoming.right_power;
+    } else {
+        right << incoming.right_power;
+    }
+
+    std::stringstream command;
+    command << left.str() << right.str() << endl;
+    cout << "SENDING THIS COMMAND: " << command.str() << endl;
+    
+    return command.str();
+}
+
+void sendMsg(robot_comm::Motor incoming) {
     int result = 0;
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -62,7 +81,9 @@ void sendMsg() {
         exit(1);
     }
 
-    const char* msg = "Jane Doe";
+    string command = returnCommand(incoming);
+    
+    const char* msg = command.c_str();
     size_t msg_length = strlen(msg);
 
     result = sendto(sock, msg, msg_length, 0, (sockaddr*)&addrDest, sizeof(addrDest));
@@ -70,22 +91,19 @@ void sendMsg() {
     std::cout << result << " bytes sent" << std::endl;
 }
 
+void motorCallBack(const robot_comm::Motor::ConstPtr& msg)
+{
+    cout << "Incoming command for: " << msg->name << endl;
+    cout << "Right power: " << msg->right_power << endl;
+    cout << "Left power: " << msg->left_power << endl;
+    sendMsg(*msg);
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "sendCommand");
     ros::NodeHandle n;
-    ros::Rate loop_rate(1);
-    
-    int count = 0;
-    while (ros::ok())
-    {
-        cout << "Sending message..." << endl;
-        sendMsg();
-        
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
-    
-    
+    ros::Subscriber sub = n.subscribe("/motor", 1000, motorCallBack);
+    ros::spin();
     return 0;
 }
